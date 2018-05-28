55fcf8b00d11740e18cecb919e08fa312f2a7213
Adam McKenna <adam-mckenna@outlook.com>
2018-05-28 15:22:24 +0100

    Added script tag reference to head; implemented mobile navigation toggle

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => { 
+    const toggles = document.getElementsByClassName('button');
+    for (let i = 0; i < toggles.length; i++) {
+        toggles[i].addEventListener('click', toggleMenu);
+    }
+})
+
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    const overlay = document.getElementById('nav-overlay');
+    const isOpen = nav.classList.contains('active');
+    overlay.classList.toggle('overlay--active');
+    nav.classList.toggle('nav--active');
+    nav.setAttribute('aria-expanded', isOpen);
+}
