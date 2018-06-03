6798dcd9559d63e6f3c1efe075e103e9155d1b02
Adam McKenna <adam-mckenna@outlook.com>
2018-06-03 21:58:57 +0100

    Loaded JavaScript files async - implemented alternative to 'DOMContentLoaded' as this would not work alongside the async attribute. Source of alternative code: https://stackoverflow.com/questions/9237044/async-loaded-scripts-with-domcontentloaded-or-load-event-handlers-not-being-call

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,28 @@
-document.addEventListener("DOMContentLoaded", init)
+HTMLDocument.prototype.ready = new Promise(resolve => {
+    if (document.readyState != "loading")
+        return resolve()
+    else
+        document.addEventListener("DOMContentLoaded", resolve())
+})
+
+document.ready.then(init)
 
 function init() {
-    const toggles = document.getElementsByClassName('nav__toggle');
+    var preloads = document.getElementsByClassName('preload')
+    for (let i = 0; i < preloads.length; i++)
+        preloads[i].classList.remove('preload')
+
+    const toggles = document.getElementsByClassName('nav__toggle')
     for (let i = 0; i < toggles.length; i++)
-        toggles[i].addEventListener('click', toggleMenu);
+        toggles[i].addEventListener('click', toggleMenu)
 }
 
 function toggleMenu() {
-    const nav = document.getElementById('nav');
-    const overlay = document.getElementById('nav-overlay');
-    const isOpen = nav.classList.contains('active');
-    overlay.classList.toggle('overlay--active');
-    nav.classList.toggle('nav--active');
-    nav.setAttribute('aria-expanded', isOpen);
+    const nav = document.getElementById('nav')
+    const overlay = document.getElementById('nav-overlay')
+    const isOpen = nav.classList.contains('active')
+    overlay.classList.toggle('overlay--active')
+    nav.classList.toggle('nav--active')
+    nav.setAttribute('aria-expanded', isOpen)
 }
+
